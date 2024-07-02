30f740441bd50021457e9a841c4b1277b2db101b
HashimotoKanna <63573845+HashimotoKanna@users.noreply.github.com>
2024-07-03 07:43:15 +0900

    update:

--- next-foods/src/app/page.tsx
+++ next-foods/src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React from 'react';
-import { useState, FormEvent } from 'react';
+import React, { useState, FormEvent } from 'react';
 import Image from 'next/image';
 
 interface Post {
   id: number;
+  name: string;
   content: string;
   imageUrl?: string;
 }
@@ -12,6 +12,7 @@ interface Post {
 const HomePage = () => {
   const [posts, setPosts] = useState<Post[]>([]);
   const [newPostContent, setNewPostContent] = useState<string>('');
+  const [newPostName, setNewPostName] = useState<string>('');
   const [newPostImage, setNewPostImage] = useState<File | null>(null);
 
   const handleSubmit = (event: FormEvent) => {
@@ -19,12 +20,14 @@ const HomePage = () => {
 
     const newPostData: Post = {
       id: posts.length + 1,
+      name: newPostName,
       content: newPostContent,
       imageUrl: newPostImage ? URL.createObjectURL(newPostImage) : undefined
     };
 
     setPosts([...posts, newPostData]);
     setNewPostContent('');
+    setNewPostName('');
     setNewPostImage(null);
   };
 
@@ -33,6 +36,15 @@ const HomePage = () => {
       <h1>みんなのご飯録</h1>
 
       <form onSubmit={handleSubmit} style={{ marginBottom: '20px' }}>
+        <div>
+          <input
+            type="text"
+            value={newPostName}
+            onChange={(e) => setNewPostName(e.target.value)}
+            placeholder="名前"
+            style={{ padding: '5px', width: '300px', marginRight: '10px' }}
+          />
+        </div>
         <div>
           <input
             type="text"
@@ -56,8 +68,17 @@ const HomePage = () => {
       <ul style={{ listStyle: 'none', paddingLeft: '0' }}>
         {posts.map((post) => (
           <li key={post.id} style={{ marginBottom: '10px', padding: '10px', border: '1px solid #ccc' }}>
+            <p>{post.name}</p>
             <p>{post.content}</p>
-            {post.imageUrl && <Image src={post.imageUrl} alt={`Post ${post.id}`} style={{ maxWidth: '100%', height: 'auto' }} />}
+            {post.imageUrl && (
+              <Image
+                src={post.imageUrl}
+                alt={`Post ${post.id}`}
+                width={500}  // ここに幅を指定
+                height={500} // ここに高さを指定
+                style={{ maxWidth: '100%', height: 'auto' }}
+              />
+            )}
           </li>
         ))}
       </ul>
